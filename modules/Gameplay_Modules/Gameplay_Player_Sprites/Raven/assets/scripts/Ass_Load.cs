13eab290ea4978f14789b2819e9aac68b1335d39
practicing01 <practicing01@gmail.com>
2014-06-13 09:37:23 -0400

    Added two more characters.

---
+++ modules/Gameplay_Modules/Gameplay_Player_Sprites/Raven/assets/scripts/Ass_Load.cs
@@ -0,0 +1,6 @@
+function Module_Raven::Ass_Load(%this)
+{
+
+%this.Ass_Image_Raven=AssetDatabase.acquireAsset("Module_Raven:Image_Raven");
+
+}
---
+++ modules/Gameplay_Modules/Gameplay_Player_Sprites/Sara/assets/scripts/Ass_Load.cs
@@ -0,0 +1,6 @@
+function Module_Sara::Ass_Load(%this)
+{
+
+%this.Ass_Image_Sara=AssetDatabase.acquireAsset("Module_Sara:Image_Sara");
+
+}

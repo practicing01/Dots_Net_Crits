eb948532f70d08416844ffb07e67178bcdb5e7dd
practicing01 <practicing01@gmail.com>
2014-07-18 07:14:54 -0400

    nano clean

--- modules/Module_Master_Server/assets/Scripts/Remote_Procedure_Calls/serverCmdMaster_Server_Query_Request.cs
+++ modules/Module_Master_Server/assets/Scripts/Remote_Procedure_Calls/serverCmdMaster_Server_Query_Request.cs
@@ -7,14 +7,14 @@ echo("Got query request from:" SPC %Client);
 for (%x=0;%x<ClientGroup.getCount();%x++)
 {
 
-%ScriptObject_Server=ClientGroup.getObject(%x);
+%GameConnection_Server=ClientGroup.getObject(%x);
 
-if (%ScriptObject_Server.Connector_Type$="Server")
+if (%GameConnection_Server.Connector_Type$="Server")
 {
 
-echo(%ScriptObject_Server.Connector_Name SPC %ScriptObject_Server.IP_Address SPC %ScriptObject_Server.Connector_Type);
+echo(%GameConnection_Server.Connector_Name SPC %GameConnection_Server.IP_Address SPC %GameConnection_Server.Connector_Type);
 
-commandToClient(%Client,'Query_Response',false,%ScriptObject_Server.Connector_Name,%ScriptObject_Server.IP_Address);
+commandToClient(%Client,'Query_Response',false,%GameConnection_Server.Connector_Name,%GameConnection_Server.IP_Address);
 
 }
 
